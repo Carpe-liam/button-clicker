58207c69c6e3478171791220911cf808fe6342e4
Alex Tyler <alexander.liam.tyler@gmail.com>
2021-08-18 14:42:24 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function loginState(element){
+    if(element.innerText == "Login"){
+        element.innerText = "Logout";
+    }
+    else{
+        element.innerText = "Login";
+    }
+}
+
+function remove(element){
+    element.remove();
+}
